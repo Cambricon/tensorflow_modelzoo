e9e5cfcca1844222a4a48514ce9699e75e12c70f
wangle <wangle@cambricon.com>
2023-03-02 07:44:28 +0000

    (feat)rm tf2mm in google_bert

--- tensorflow2/built-in/NaturalLanguageProcessing/google_bert/env.sh
+++ tensorflow2/built-in/NaturalLanguageProcessing/google_bert/env.sh
@@ -1,2 +1,3 @@
 export MODEL_DIR=/data/tensorflow/training/datasets/CMCC/uncased_L-12_H-768_A-12
 export SQUAD_DIR=/data/tensorflow/training/datasets/Bert/SQuAD
+export INFER_CKPT=/data/tensorflow/training/models/test/google_bert_tf2/single/model.ckpt-5474
