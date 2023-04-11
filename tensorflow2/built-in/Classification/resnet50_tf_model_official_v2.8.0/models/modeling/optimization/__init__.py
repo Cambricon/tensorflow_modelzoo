54394aba6d7003a1ccad68086062668601326027
mashaobin <mashaobin@cambricon.com>
2023-04-11 11:34:02 +0000

    feat(tf2): drop some network

--- tensorflow2/built-in/Classification/resnet50_tf_model_official_v2.8.0/models/modeling/optimization/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Optimization package definition."""
-
-# pylint: disable=wildcard-import
-from models.modeling.optimization.configs.learning_rate_config import *
-from models.modeling.optimization.configs.optimization_config import *
-from models.modeling.optimization.configs.optimizer_config import *
-from models.modeling.optimization.ema_optimizer import ExponentialMovingAverage
-from models.modeling.optimization.optimizer_factory import OptimizerFactory
