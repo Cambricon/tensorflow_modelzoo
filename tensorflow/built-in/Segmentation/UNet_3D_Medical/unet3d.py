c3acc7c208fa47e02e367ce731bad220b7d933ea
wangle <wangle@cambricon.com>
2023-04-21 06:07:52 +0000

    (feat)add support of performance record for evaluation

--- tensorflow/built-in/Segmentation/UNet_3D_Medical/unet3d.py
+++ tensorflow/built-in/Segmentation/UNet_3D_Medical/unet3d.py
@@ -19,6 +19,7 @@ import numpy as np
 import tensorflow as tf
 import horovod.tensorflow as hvd
 import sys
+
 sys.path.append("./models")
 sys.path.append("./models/runtime")
 sys.path.append("./models/model")
@@ -26,13 +27,20 @@ sys.path.append("./models/model")
 from models.dataset.data_loader import Dataset, CLASSES
 from models.runtime.hooks import get_hooks, ProfilingHook, TrainingHook
 from models.runtime.arguments import PARSER
-from models.runtime.setup import prepare_model_dir, build_estimator, set_flags, get_logger
+from models.runtime.setup import (
+    prepare_model_dir,
+    build_estimator,
+    set_flags,
+    get_logger,
+)
 
 
 def parse_evaluation_results(result):
     data = {CLASSES[i]: result[CLASSES[i]] for i in range(len(CLASSES))}
-    data['MeanDice'] = sum([result[CLASSES[i]] for i in range(len(CLASSES))]) / len(CLASSES)
-    data['WholeTumor'] = result['WholeTumor']
+    data["MeanDice"] = sum([result[CLASSES[i]] for i in range(len(CLASSES))]) / len(
+        CLASSES
+    )
+    data["WholeTumor"] = result["WholeTumor"]
     return data
 
 
@@ -45,18 +53,23 @@ def main():
     logger = get_logger(params)
 
     if params.use_performance and params.use_profiler:
-        raise ValueError("You can only set use_profiler or use_performance, not at the same time, otherwise the e2e time will be worse")
+        raise ValueError(
+            "You can only set use_profiler or use_performance, not at the same time, otherwise the e2e time will be worse"
+        )
 
     if params.use_performance:
         from record_time import TimeHook, write_json
+
         global TimeHook
         global write_json
 
-    dataset = Dataset(data_dir=params.data_dir,
-                      batch_size=params.batch_size,
-                      fold_idx=params.fold,
-                      n_folds=params.num_folds,
-                      params=params)
+    dataset = Dataset(
+        data_dir=params.data_dir,
+        batch_size=params.batch_size,
+        fold_idx=params.fold,
+        n_folds=params.num_folds,
+        params=params,
+    )
 
     estimator = build_estimator(params=params, model_dir=model_dir)
 
@@ -65,68 +78,104 @@ def main():
     else:
         max_steps = params.max_steps // (1 if params.benchmark else hvd.size())
 
-    if 'train' in params.exec_mode:
+    if "train" in params.exec_mode:
         training_hooks = get_hooks(params, logger)
         if params.use_performance and hvd.rank() == 0:
             time_hooks = TimeHook()
             training_hooks.append(time_hooks)
         if params.use_profiler and hvd.rank() == 0:
-            timeline_hook = tf.estimator.ProfilerHook(save_steps=5, output_dir='./profiler')
+            timeline_hook = tf.estimator.ProfilerHook(
+                save_steps=5, output_dir="./profiler"
+            )
             training_hooks.append(timeline_hook)
         estimator.train(
-            input_fn=dataset.train_fn,
-            steps=max_steps,
-            hooks=training_hooks)
+            input_fn=dataset.train_fn, steps=max_steps, hooks=training_hooks
+        )
         if params.use_performance and hvd.rank() == 0:
             write_json("summary", params.batch_size * hvd.size(), time_hooks.times)
 
-    if 'evaluate' in params.exec_mode:
-        result = estimator.evaluate(input_fn=dataset.eval_fn, steps=dataset.eval_size)
+    if "evaluate" in params.exec_mode:
+        eval_hooks = get_hooks(params, logger)
+        need_record_flag = False
+        if params.use_performance:
+            if params.use_horovod:
+                if hvd.rank() == 0:
+                    need_record_flag = True
+            else:
+                need_record_flag = True
+        if need_record_flag != False:
+            time_hooks = TimeHook()
+            eval_hooks.append(time_hooks)
+
+        result = estimator.evaluate(
+            input_fn=dataset.eval_fn, steps=dataset.eval_size, hooks=eval_hooks
+        )
         data = parse_evaluation_results(result)
         if hvd.rank() == 0:
             logger.log(step=(), data=data)
+        if need_record_flag == 1:
+            write_json("summary", params.batch_size * hvd.size(), time_hooks.times)
+        elif need_record_flag == 2:
+            write_json("summary", params.batch_size, time_hooks.times)
 
-    if 'predict' == params.exec_mode:
+    if "predict" == params.exec_mode:
         inference_hooks = get_hooks(params, logger)
         if hvd.rank() == 0:
-            count = 1 if not params.benchmark else 2 * params.warmup_steps * params.batch_size // dataset.test_size
+            count = (
+                1
+                if not params.benchmark
+                else 2 * params.warmup_steps * params.batch_size // dataset.test_size
+            )
             predictions = estimator.predict(
-                input_fn=lambda: dataset.test_fn(count=count,
-                                                 drop_remainder=params.benchmark), hooks=inference_hooks)
+                input_fn=lambda: dataset.test_fn(
+                    count=count, drop_remainder=params.benchmark
+                ),
+                hooks=inference_hooks,
+            )
 
             for idx, p in enumerate(predictions):
-                volume = p['predictions']
+                volume = p["predictions"]
                 if not params.benchmark:
-                    np.save(os.path.join(params.model_dir, "vol_{}.npy".format(idx)), volume)
+                    np.save(
+                        os.path.join(params.model_dir, "vol_{}.npy".format(idx)), volume
+                    )
 
-    if 'debug_train' == params.exec_mode:
+    if "debug_train" == params.exec_mode:
         hooks = [hvd.BroadcastGlobalVariablesHook(0)]
         if hvd.rank() == 0:
-            hooks += [TrainingHook(log_every=params.log_every,
-                                   logger=logger,
-                                   tensor_names=['total_loss_ref:0']),
-                      ProfilingHook(warmup_steps=params.warmup_steps,
-                                    global_batch_size=hvd.size() * params.batch_size,
-                                    logger=logger,
-                                    mode='train')]
+            hooks += [
+                TrainingHook(
+                    log_every=params.log_every,
+                    logger=logger,
+                    tensor_names=["total_loss_ref:0"],
+                ),
+                ProfilingHook(
+                    warmup_steps=params.warmup_steps,
+                    global_batch_size=hvd.size() * params.batch_size,
+                    logger=logger,
+                    mode="train",
+                ),
+            ]
 
-        estimator.train(
-            input_fn=dataset.synth_train_fn,
-            steps=max_steps,
-            hooks=hooks)
+        estimator.train(input_fn=dataset.synth_train_fn, steps=max_steps, hooks=hooks)
 
-    if 'debug_predict' == params.exec_mode:
+    if "debug_predict" == params.exec_mode:
         if hvd.rank() == 0:
-            hooks = [ProfilingHook(warmup_steps=params.warmup_steps,
-                                   global_batch_size=params.batch_size,
-                                   logger=logger,
-                                   mode='inference')]
+            hooks = [
+                ProfilingHook(
+                    warmup_steps=params.warmup_steps,
+                    global_batch_size=params.batch_size,
+                    logger=logger,
+                    mode="inference",
+                )
+            ]
             count = 2 * params.warmup_steps
-            predictions = estimator.predict(input_fn=lambda: dataset.synth_predict_fn(count=count),
-                                            hooks=hooks)
+            predictions = estimator.predict(
+                input_fn=lambda: dataset.synth_predict_fn(count=count), hooks=hooks
+            )
             for p in predictions:
-                _ = p['predictions']
+                _ = p["predictions"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
